1a3997afe810bdcedc449783fcda9220d5ab9590
Andy Nowacki <a.nowacki@leeds.ac.uk>
2020-08-10 09:56:59 +0100

    Test new implementation of is_stable
    - Fix bugs in is_stable
    - Accept either any Matrix or ECs
    - Make passing a non-6x6 matrix an ArgumentError
    - Test for logging of a warning if a matrix is not symmetrical

--- test/properties.jl
+++ test/properties.jl
@@ -1,6 +1,7 @@
 # Test calculations of properties of tensors
 
 using Test, CIJ
+using LinearAlgebra: Symmetric
 
 @testset "Properties" begin
     @testset "Is iso" begin
@@ -17,12 +18,26 @@ using Test, CIJ
     @testset "Is stable" begin
         let c = CIJ.iso(vp=8000, vs=4500), c′ = CIJ.iso(vp=8000, vs=0)
             @test is_stable(c)
+            @test is_stable(c.data)
+            @test is_stable(Array(c))
+            @test is_stable(Array(c.data))
             @test !is_stable(c′)
+            @test !is_stable(c′.data)
+            @test !is_stable(Array(c′))
+            @test !is_stable(Array(c′.data))
+
             c[1,1] = -c[1,1]
             @test !is_stable(c)
             c[1,1] = c[2,2]
             c[1,3] = c[1,2] = c[2,3] = c[1,1]
             @test !is_stable(c)
+        end
+        @testset "Catch wrong exception" begin
+            let c = CIJ.ol()[1]
+                @test_logs (:warn, "matrix not symmetrical: using upper half only"
+                    ) is_stable(rand(6,6))
+                @test_throws ArgumentError is_stable(rand(5,5))
+            end
         end
     end
 end
