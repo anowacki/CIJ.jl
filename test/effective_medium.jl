f4e7b131f50b774e123a1290e1478c38d87b85bb
Andy Nowacki <a.nowacki@leeds.ac.uk>
2026-03-13 15:49:31 +0000

    Fix bug in pitl
    `pitl` had the same error in using in-place division as
    `tandon_and_weng` (0ab8653430d17288baeb7dea3a3cf36fe7b2b159).
    Fix this in the same way.

--- test/effective_medium.jl
+++ test/effective_medium.jl
@@ -2,6 +2,23 @@ using CIJ
 using Test
 
 @testset "Effective media" begin
+    @testset "pitl" begin
+        C, ρ = CIJ.pitl(0.1, 2000, 1000, 1500, 0.01, 1500, 100, 1000)
+        @test ρ ≈ (0.1*1500 + 0.01*1000)/0.11
+        @test all(
+            .≈(
+                C,
+                [ 3.87762e6  2.00137e6  1.95105e6      0.0      0.0       0.0
+                  2.00137e6  3.87762e6  1.95105e6      0.0      0.0       0.0
+                  1.95105e6  1.95105e6  3.58224e6      0.0      0.0       0.0
+                  0.0        0.0        0.0        70898.4      0.0       0.0
+                  0.0        0.0        0.0            0.0  70898.4       0.0
+                  0.0        0.0        0.0            0.0      0.0  938125.0];
+                  atol=100
+            )
+        )
+    end
+
     @testset "tandon_and_weng" begin
         @testset "Water-filled cracks" begin
             C, ρ = tandon_and_weng(3240, 1800, 3800, 0.01, 0.01, 1500, 0, 1000)
