2ac159954914e349e97212ee78c83e1f1ebbd88b
Andy Nowacki <a.nowacki@leeds.ac.uk>
2026-03-25 21:14:59 +0000

    Refactor arguments for thomsen_cracks and thomsen_fluid_factor
    The arguments for `thomsen_cracks` and `thomsen_fluid_factor` as
    originally set in 97fa18d0fbeb87187dd did not really mirror those used
    elsewhere in CIJ.jl, so update them with the following principles in
    mind:
    
    1. Density should only be provided (and elastic constants returned in
       units of pressure) if necessary, e.g. when densities of matrix and
       inclusions matter and give a different overall density.  Otherwise
       density should not be given and density-normalised tensors returned.
    2. If density is passed, it should come after Vp and Vs, but before
       dimensionless parameters.
    3. Parameters for the background medium should come first, then
       parameters for the fractures, and finally the fluid.
    
    Therefore update:
    - `thomsen_cracks`:
      - Rename arguments to be simpler (non-breaking change)
      - Add `delta_option` to pass to `thomsen_vti`
      - Change argument order to match the principles above
      - Call the new `thomsen_vti` function to generate the actual ECs
    - `thomsen_fluid_factor`:
      - Rename arguments
      - Change argument order
    
    As well as the above, expand docstrings to include definitions of e.g.
    fracture density.

--- src/effective_medium.jl
+++ src/effective_medium.jl
@@ -261,151 +261,164 @@ grechka_cracks(C, ξ::AbstractVector, ϕ::AbstractVector=zeros(ξ, eltype(C))) =
 @doc (@doc grechka_cracks!) grechka_cracks
 
 """
-    thomsen_cracks(vp_iso, vs_iso, flu_fac, frac_den) -> C
+    thomsen_cracks(vp, vs, flu_fac, frac_den; delta_option=:weak_mensch) -> C
 
-Create tensor for isotropic and porous rock containing a single fracture set using the Thomsen's (1995)
-formulation assuming relatively low-frequency and low-porosity (less-restrictive equations also provided).
+Create **density-normalised** elastic tensor for isotropic and porous rock
+containing a single fracture set using Thomsen's (1995) formulation
+assuming relatively low-frequency and low-porosity.  (Less restrictive equations
+are also provided by Thomsen but not implemeted here yet.)
 
-`dens_iso` is the density of the porous host rock.
+`vp` is the isotropic P-velocity of the porous host rock.
 
-`vp_iso` is the isotropic P-velocity of the porous host rock.
+`vs` is the isotropic S-velocity of the porous host rock.
 
-`vs_iso` is the isotropic S-velocity of the porous host rock.
+`flu_fac` is Thomsen's (1995) fluid factor parameter (Eq. 7a multiplied
+by 1 - ratio of pore fluid-to-solid bulk moduli).
+Value is confined to [0.0, 1.0]; weaker pore fluids correspond to larger
+`flu_fac` (e.g., gas ≈ 0.0).
 
-`flu_fac` is Thomsen's (1995) fluid factor parameter (Eq. 7a multiplied by 1 - ratio of pore fluid-to-solid bulk moduli).
-Value is confined to [0.0, 1.0]; Weaker pore fluids correspond to larger flu_fac (e.g., gas ≈ 0.0).
+`frac_den` is the fracture density ``\\eta_c`` (Eq. 3), defined as
+```math
+    \\eta_c = N_v \\langle a^3 \\rangle / 8,
+```
+where ``N_v`` is the number density of fractures per unit volume and
+``\\langle a^3\\rangle`` is the mean cubed fracture radius.
 
-`frac_den` is the fracture density (see Eq. 3a-3b for definition.)
+### Keyword arguments
 
-#### References
+- `delta_option = :weak_mensch`: Method to calculate C₁₃ from Thomsen's δ or
+  δ* parameter, passed to [`thomsen_vti`](@ref).  See [`thomsen_vti`](@ref)
+  for details.
 
-1. Thomsen, L. (1995). Elastic anisotropy due to aligned cracks in porous rock1. Geophysical Prospecting, 43(6), 805-829.
+### References
+
+- Thomsen, L. (1995). Elastic anisotropy due to aligned cracks in porous rock.
+  Geophysical Prospecting, 43(6), 805-829.
 """
-function thomsen_cracks(dens_iso, vp_iso, vs_iso, flu_fac, frac_den; delta_option = "weak_mensch")
+function thomsen_cracks(vp, vs, flu_fac, frac_den; delta_option=:weak_mensch)
+    if !(0 <= flu_fac <= 1)
+        throw(ArgumentError("flu_fac must be in the range 0 to 1."))
+    end
+
     # Define Thomsen's parameters
-    vp_vs_ratio = vp_iso/vs_iso
-    ν = (0.5/(1.0 - ((1.0/vp_vs_ratio)^2))) - (1.0/(((vp_vs_ratio)^2) - 1.0))
+    vp_vs_ratio = vp/vs
+    ν = 0.5/(1 - 1/vp_vs_ratio^2) - 1/(vp_vs_ratio^2 - 1)
     ϵ = (8/3)*flu_fac*frac_den # Eq. 6a; Always positive!
-    γ = (8/3)*frac_den*(1.0 - ν)/(2.0 - ν) # Eq. 6b; Always positive! Note γ > ϵ for stiffer pore fluids and low aspect ratio
-    δ = 2.0*(1.0 - ν)*ϵ - 2.0*γ*(1.0 - 2.0*ν)/(1.0 - ν) # Eq. 6c; Can reasonably be ±
-    α = vp_iso/sqrt(1.0 + 2.0*(((1.0 - ν)^2)/(1.0 - 2.0*ν))*ϵ)
-    β = vs_iso/sqrt(1.0 + 2.0*γ)
+    γ = (8/3)*frac_den*(1 - ν)/(2 - ν) # Eq. 6b; Always positive! Note γ > ϵ for stiffer pore fluids and low aspect ratio
+    δ = 2*(1 - ν)*ϵ - 2*γ*(1 - 2*ν)/(1 - ν) # Eq. 6c; Can reasonably be ±
+    α = vp/sqrt(1 + 2*(((1 - ν)^2)/(1 - 2*ν))*ϵ)
+    β = vs/sqrt(1 + 2*γ)
 
-    return thomsen_to_elastic(dens_iso, vp_iso, vs_iso, ϵ, γ, δ; delta_option = delta_option)
+    thomsen_vti(vp, vs, ϵ, γ, δ; delta_option=delta_option)
 end
 
 """
-    thomsen_cracks(vp_iso, vs_iso, flu_fac, frac_den) -> C
+    thomsen_cracks(vp, vs, density, porosity, frac_aspect_ratio, frac_density, Kf; delta_option=:weak_mensch) -> C
 
-Create tensor for isotropic and porous rock containing a single fracture set using the Thomsen's (1995)
-formulation assuming relatively low-frequency and low-porosity (less-restrictive equations also provided).
+Determine the density-normalised elastic constants using fracture density,
+aspect ratio and fluid bulk modulus rather than the fluid factor.
 
-`dens_iso` is the density of the porous host rock.
+`vp` is the isotropic P-velocity of the porous host rock.
 
-`vp_iso` is the isotropic P-velocity of the porous host rock.
+`vs` is the isotropic S-velocity of the porous host rock.
 
-`vs_iso` is the isotropic S-velocity of the porous host rock.
+`density` is the density of the porous host rock.
 
-`frac_aspect_ratio` is the aspect ratio of the fractures (thickness/diameter)
+`porosity` is the background porosity of the host rock.
 
-`frac_density` is the fracture density (see Eq. 3a-3b for definition.)
+`frac_aspect_ratio` is the aspect ratio of the fractures (thickness/diameter).
 
-`iso_porosity` is the background porosity of the host rock.
+`frac_density` is the fracture density (see Eq. 3 for definition).
 
 `Kf` is the fluid bulk modulus (ρ_fluid*vp_fluid²)
 
-#### References
-
-1. Thomsen, L. (1995). Elastic anisotropy due to aligned cracks in porous rock1. Geophysical Prospecting, 43(6), 805-829.
+!!! note
+    The units of `vp`, `vs`, `density` and `Kf` must be consistent for `C` to
+    be meaningful; e.g. velocities should be in m/s, density should be in kg/m³,
+    and `Kf` should be in Pa.  No check is made on this.
 """
-function thomsen_cracks(dens_iso, vp_iso, vs_iso, frac_aspect_ratio, frac_density, iso_porosity, Kf; delta_option = "weak_mensch")
-    flu_fac = thomsen_fluid_factor(dens_iso, vp_iso, vs_iso, frac_aspect_ratio, frac_density, iso_porosity, Kf)
-    return thomsen_cracks(dens_iso, vp_iso, vs_iso, flu_fac, frac_density; delta_option = delta_option)
+function thomsen_cracks(
+    vp, vs, density, porosity, frac_aspect_ratio, frac_density, Kf;
+    delta_option=:weak_mensch
+)
+    flu_fac = thomsen_fluid_factor(
+        vp, vs, density, porosity, frac_aspect_ratio, frac_density, Kf
+    )
+
+    thomsen_cracks(vp, vs, flu_fac, frac_density; delta_option=delta_option)
 end
 
 """
-    thomsen_fluid_factor(density_iso, vp_iso, vs_iso, frac_aspect_ratio, frac_density, iso_poro, Kf) -> Float
+    thomsen_cracks(vp, vs, density, porosity, frac_aspect_ratio, frac_density, vp_fluid, density_fluid) -> C
 
-Compute fluid factor from Thomsen's (1995).
-
-`density_iso` is the density of the porous host rock.
-
-`vp_iso` is the isotropic P-velocity of the porous host rock.
-
-`vs_iso` is the isotropic S-velocity of the porous host rock.
-
-`frac_aspect_ratio` is the aspect ratio of the fractures (thickness/diameter)
-
-`frac_density` is the fracture density (see Eq. 3a-3b for definition.)
-
-`iso_poro` is the background porosity of the host rock.
-
-`Kf` is the fluid bulk modulus (ρ_fluid*vp_fluid²)
-
-#### References
-
-1. Thomsen, L. (1995). Elastic anisotropy due to aligned cracks in porous rock1. Geophysical Prospecting, 43(6), 805-829.
+Determine constants using fluid P-wave velocity `vp_fluid` and density
+`density_fluid` rather than specifying bulk modulus directly.
 """
-function thomsen_fluid_factor(density_iso, vp_iso, vs_iso, frac_aspect_ratio, frac_density, iso_poro, Kf)
+function thomsen_cracks(
+    vp, vs, density, porosity, frac_aspect_ratio, frac_density, vp_fluid, density_fluid;
+    delta_option=:weak_mensch
+)
+    Kf = density_fluid*vp_fluid^2
+    thomsen_cracks(
+        vp, vs, density, porosity, frac_aspect_ratio, frac_density, Kf;
+        delta_option=delta_option
+    )
+end
+
+"""
+    thomsen_fluid_factor(vp, vs, porosity, density, frac_aspect_ratio, frac_density, Kf)
+
+Compute fluid factor from Thomsen's (1995) theory of elasticity due to aligned
+fractures in a porous medium.
+
+`vp` is the isotropic P-velocity of the porous host rock.
+
+`vs` is the isotropic S-velocity of the porous host rock.
+
+`density` is the density of the porous host rock.
+
+`porosity` is the background porosity of the host rock.
+
+`frac_aspect_ratio` is the aspect ratio of the fractures (thickness/diameter).
+Values less than one describe flat 'pancake' fractures, while ratios bigger
+than 1 are for 'rugby ball'-shaped fractures.
+
+`frac_density` is the fracture density ``\\eta_c``, defined by Thomsen (1995)
+Eq. 3 as
+```math
+    \\eta_c = N_v \\langle a^3 \\rangle / 8,
+```
+where ``N_v`` is the number density of fractures per unit volume and
+``\\langle a^3\\rangle`` is the mean cubed fracture radius.
+
+`Kf` is the fluid bulk modulus (ρ_fluid*vp_fluid²).
+
+!!! note
+    The units of `vp`, `vs`, `density` and `Kf` must be consistent for `f` to
+    be meaningful; e.g. velocities should be in m/s, density should be in kg/m³,
+    and `Kf` should be in Pa.  No check is made on this.
+
+### References
+
+- Thomsen, L. (1995). Elastic anisotropy due to aligned cracks in porous rock.
+  Geophysical Prospecting, 43(6), 805-829.
+"""
+function thomsen_fluid_factor(
+    vp, vs, density, porosity, frac_aspect_ratio, frac_density, Kf
+)
     # Derived elastic constants
-    Gs = density_iso*(vs_iso^2) # Solid shear modulus
-    Ks = density_iso*(vp_iso^2) - (4/3)*Gs # Solid bulk modulus
-    νs = (3.0*Ks - 2.0*Gs)/(6.0*Ks + 2.0*Gs) # Solid poisson ratio
+    Gs = density*vs^2 # Solid shear modulus
+    Ks = density*vp^2 - (4/3)*Gs # Solid bulk modulus
+    νs = (3*Ks - 2*Gs)/(6*Ks + 2*Gs) # Solid poisson ratio
     frac_poro = (4/3)*pi*frac_aspect_ratio*frac_density # Fracture porosity
 
-    # Fluid-influence factor terms 
-    Ac = (16/9)*(1.0 - (νs^2))/(1.0 - 2.0*νs) # Eq. A13b
-    Ap = (3/2)*(1.0 - νs)/(1.0 - 2.0*νs) # Eq. A30b
-    Dcp = 1.0/(1.0 - (Kf/Ks) + (Kf/(Ks*(iso_poro + frac_poro)))*(Ap*iso_poro + Ac*frac_density)) # Eq. 7a
+    # Fluid-influence factor terms
+    Ac = (16/9)*(1 - νs^2)/(1 - 2*νs) # Eq. A13b
+    Ap = (3/2)*(1 - νs)/(1 - 2*νs) # Eq. A30b
+    Dcp = 1/(
+        1 - Kf/Ks
+        + (Kf/(Ks*(porosity + frac_poro)))*(Ap*porosity + Ac*frac_density)
+    ) # Eq. 7a
 
-    return (1.0 - (Kf/Ks))*Dcp
-end
-
-"""
-    thomsen_to_elastic(ρ, α, β, ϵ, γ, δ) -> C
-
-Returns elastic constants (EC type) from Thomsen's (1986) parameters.
-
-`ρ` is the rock density.
-
-`α`, `β`, `ϵ`, `γ`, `δ` are Thomsen's (1986) parameters.
-
-`delta_option` is a keyword specifying which relationship to use in computing
-Thomsen's δ parameter. Options are:
-= "weak_thomsen" for Thomsen's (1986) weak anisotropy approximation
-= "weak_mensch" from Mensch and Rasolofosaon (1997) for weak anisotropy using more accurate expansion
-= "exact" for Thomsen's (1986) exact anisotropy equations
-
-#### References
-
-1. Thomsen, L. (1986). Weak elastic anisotropy. Geophysics, 51(10), 1954-1966.
-
-2. Mensch, T., & Rasolofosaon, P. (1997). Elastic-wave velocities in anisotropic media of arbitrary
-   symmetry—generalization of Thomsen's parameters ε, δ and γ. Geophysical Journal International, 128(1), 43-64.
-"""
-function thomsen_to_elastic(ρ, α, β, ϵ, γ, δ; delta_option = "weak_mensch")
-    c33 = ρ*α^2
-    c44 = ρ*β^2
-    c11 = (1.0 + 2.0*ϵ)*c33
-    c66 = (1.0 + 2.0*γ)*c44
-    if delta_option == "weak_thomsen" # Weak formula (Eq. 17) to be used with Thomsen's weak anisotropy equations (Eq. 16) 
-        c13 = sqrt(2.0*δ*c33*(c33 - c44) + ((c33 - c44)^2)) - c44
-    elseif delta_option == "weak_mensch" # Alternative (and generally more accurate) form from Mensch & Rasolofosaon (1997)
-        c13 = δ*c33 + c33 - 2.0*c44
-    elseif delta_option == "exact" # Exact formula (Eq. 8c) to be used with Thomsen's exact anisotropy equations (Eq. 10)
-        c13 = sqrt(δ*(c33^2) + 0.5*(c33 - c44)*(c11 + c33 - 2.0*c44)) - c44
-    else
-        error("Unknown delta option: "*delta_option)
-    end
-    c12 = c11 - 2.0*c66
-
-    C = EC([
-        c11 c12 c13 0.0 0.0 0.0;
-        c12 c11 c13 0.0 0.0 0.0;
-        c13 c13 c33 0.0 0.0 0.0;
-        0.0 0.0 0.0 c44 0.0 0.0;
-        0.0 0.0 0.0 0.0 c44 0.0;
-        0.0 0.0 0.0 0.0 0.0 c66 
-    ])
-    return C
+    return (1 - Kf/Ks)*Dcp
 end
